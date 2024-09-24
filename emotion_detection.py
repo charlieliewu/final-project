96dec8d6d4605b3e258fee2e5584e0f4a2325c44
charlieliewu <155046190+charlieliewu@users.noreply.github.com>
2024-09-24 13:59:42 +0800

    emotion_detection.py

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,37 +1,16 @@
 import requests
 import json
 
-# Emotion Detection Function        
-def emotion_detector(text_to_analyze):  
-    # Watson NLP Emotion Detection API URL
-    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'  
-    
-    # requests
+def emotion_detector(text_to_analyze):
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     headers = {
-        "grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock",    
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
+        "grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"
     }
-    
-    # input json
     input_json = {
         "raw_document": {
-            "text": text_to_analyze  
+            "text": text_to_analyze
         }
     }
-
-    # Sending a POST request
-    response = requests.post(url, headers=headers, data=json.dumps(input_json))    
-    
-    # Returns the text attribute in the response
-    if response.status_code == 200:
-        return response.json()  
-    else:
-        return {"error": "Failed to fetch emotion prediction"}  
-
-# Test code
-if __name__ == "__main__":
-    test_text = "I like this new technology."
-    result = emotion_detector(test_text)
-    print(result)
-
-
+    response = requests.post(url, headers=headers, data=json.dumps(input_json))
+    return response.text
