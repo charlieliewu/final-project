acd318a927a6f5e889b65a3e9b3f0399b4e23b66
charlieliewu <155046190+charlieliewu@users.noreply.github.com>
2024-09-29 16:31:41 +0800

    emotion_detection.py

--- emotion_detection.py
+++ emotion_detection.py
@@ -5,11 +5,11 @@ def emotion_detector(text_to_analyze):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     headers = {
         "Content-Type": "application/json",
-        "grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"
+        "grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"  
     }
     input_json = {
         "raw_document": {
-            "text": text_to_analyze
+            "text": I love this new technology        
         }
     }
     response = requests.post(url, headers=headers, data=json.dumps(input_json))
